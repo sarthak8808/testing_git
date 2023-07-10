910c1d2d252e4feaeaa281b1b9da64007b40c0b9
sarthak8808 <136673621+sarthak8808@users.noreply.github.com>
2023-07-10 12:54:39 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,16 +4,18 @@ from config import *
 from scripts.Evaluation import *
 from scripts.register_model import *
 
-src_file_path=src_file_path
-
-df=read_data(src_file_path)
+#src_file_path=src_file_path
+file_path=r"dbfs:/FileStore/tables/creditcard.csv"
+pandas_df = spark.read.csv(file_path, header=True, inferSchema=True)
+df = pandas_df.toPandas()
+#df=read_data(src_file_path)
 #print(df.head())
 
 X,y=seperate_d_I_values(df)
 
 import mlflow
 mlflow.autolog()
-experiment_name="Xgboost_classification"
+experiment_name=r"/testing"
 run_name="xgboost_fraud_detect"
 print(run_name)
 mlflow.set_experiment(experiment_name)
@@ -28,5 +30,5 @@ best_xgbc=best_Model(X_train, y_train,scale_pos_weight)
 
 #model_registry(best_model=best_xgbc,run_params=best_parms,src_file_path=src_file_path)
 
-evaluate(best_xgbc,X_test,y_test)
+#evaluate(best_xgbc,X_test,y_test)
 
